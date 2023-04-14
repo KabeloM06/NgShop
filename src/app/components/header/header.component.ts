604b50d59236eb5f0f9dfb85318c9ad108de33ce
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-04-14 20:21:13 +0200

    end of day code

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
+  private _cart: Cart ={items: []};
+  itemsQuantity = 0;
+
+  @Input()
+  get cart(): Cart{
+    return this._cart;
+  }
+
+  set cart(cart: Cart){
+    this._cart = cart;
+
+    this.itemsQuantity = cart.items.map((item) => item.quantity).reduce((prev, current) => prev + current, 0);
+  }
 
   constructor() { }
 

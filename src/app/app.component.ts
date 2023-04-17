55a41d38eb3f4fd5390b6fc68a5ce7120efcacb9
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2023-04-17 17:57:36 +0200

    Items added to cart from home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from './services/cart.service';
+import { Cart } from './models/cart.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ngShop';
+
+  cart: Cart = {items: []};
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+      this.cartService.cart.subscribe((_cart) => {
+        this.cart = _cart;
+      });
+  }
 }
